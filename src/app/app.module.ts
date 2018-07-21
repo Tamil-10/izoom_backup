d0fff8e642f8b74138d8be7374751b9712c60a06
unknown <itsupport@NVS005-PC.studios.local>
2018-07-21 17:42:38 +0530

    Add all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
+import { NgxStripeModule } from 'ngx-stripe';
 import { AppComponent } from './app.component';
 
-
+import { ProductconfigComponent } from './product/productconfig/productconfig.component';
+import { ManageproductconfigComponent } from './product/manageproductconfig/manageproductconfig.component';
+import { AppRoutingModule } from './app.routing';
+import { SearchproductComponent } from './product/searchproduct/searchproduct.component';
+import { PollconfigComponent } from './poll/pollconfig/pollconfig.component';
+import { ManagepollconfigComponent } from './poll/managepollconfig/managepollconfig.component';
+import { PublishedpollsComponent } from './poll/publishedpolls/publishedpolls.component';
+import { PaymentgatwayComponent } from './payment/paymentgatway/paymentgatway.component';
+import { CartComponent } from './product/cart/cart.component';
+import { OrdersummaryComponent } from './product/ordersummary/ordersummary.component';
+import { ProductService } from './service/product.service';
+import { GiftsComponent } from './gifts/gifts.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductconfigComponent,
+    ManageproductconfigComponent,
+    SearchproductComponent,
+    PollconfigComponent,
+    ManagepollconfigComponent,
+    PublishedpollsComponent,
+    PaymentgatwayComponent,
+    CartComponent,
+    OrdersummaryComponent,
+    GiftsComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    NgxStripeModule.forRoot('***your-stripe-publishable key***')
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
